31bb7d74bc2850fa34973d69919ccb20e31bf3fe
Raghav Chalageri <raghavc2004@gmail.com>
2026-05-06 15:41:00 -0400

    Initial Commit

---
+++ RichRank/RichRankApp.swift
@@ -0,0 +1,17 @@
+//
+//  RichRankApp.swift
+//  RichRank
+//
+//  Created by Raghav Chalageri on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct RichRankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RichRankTests/RichRankTests.swift
@@ -0,0 +1,19 @@
+//
+//  RichRankTests.swift
+//  RichRankTests
+//
+//  Created by Raghav Chalageri on 5/6/26.
+//
+
+import Testing
+@testable import RichRank
+
+struct RichRankTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
